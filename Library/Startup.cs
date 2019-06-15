b2fa34d4a660eb6ab3a2dda59d36957ced5e2764
Usman Ali <usmanali4073@gmail.com>
2019-06-14 23:20:53 -0400

    startup

--- Library/Startup.cs
+++ Library/Startup.cs
@@ -32,7 +32,7 @@ namespace Library
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //var connectionString = Configuration["connectionStrings:libraryDBConnectionString"];
+            var connectionString = Configuration["connectionStrings:libraryDBConnectionString"];
 
 
             services.AddDbContext<LibraryContext>(o =>
